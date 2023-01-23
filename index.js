a4fd0cf3305877685aef38e651b23da12a5bd3ed
Philip <17368112+vHeemstra@users.noreply.github.com>
2023-01-23 17:32:55 +0100

    Added more granular verbose option
    * You can now choose between verbose modes:
      - `0` - No logging at all
      - `1` - Essentials only (same as `true`)
      - `2` - All logs
    
    * This is backwards compatible with:
      - the boolean verbose option
      - and the `--verbose` command-line argument override.

--- index.js
+++ index.js
@@ -1,68 +1,73 @@
-const path = require('path')
-const log = require('fancy-log')
-const PluginError = require('plugin-error')
-const through = require('through2-concurrent')
-const chalk = require('chalk')
-const plur = require('plur')
-const gettextParser = require('gettext-parser')
+const path = require("path");
+const log = require("fancy-log");
+const PluginError = require("plugin-error");
+const through = require("through2-concurrent");
+const chalk = require("chalk");
+const plur = require("plur");
+const gettextParser = require("gettext-parser");
 
-const PLUGIN_NAME = 'gulp-compile-translation-files'
+const PLUGIN_NAME = "gulp-compile-translation-files";
 
 module.exports = (opts) => {
+	const validExts = [".po"];
 
-  const validExts = ['.po']
+	if (process.argv.indexOf("--verbose") !== -1) {
+		opts.verbose = 2;
+	} else {
+		opts.verbose = typeof opts.verbose !== "undefined" ? Number(opts.verbose) : 1;
+	}
 
-	opts = Object.assign({
-		verbose: process.argv.indexOf('--verbose') !== -1
-	}, opts)
+	let totalFiles = 0;
 
-  let totalFiles = 0;
-
-	return through.obj({
-		maxConcurrency: 8
-	}, (file, enc, cb) => {
-
-		if (file.isNull()) {
-			cb(null, file)
-			return
-		}
-
-		if (file.isStream()) {
-			return cb(new PluginError(PLUGIN_NAME, 'Streaming not supported'))
-		}
-
-		if (validExts.indexOf(path.extname(file.path).toLowerCase()) === -1) {
-
-      if (opts.verbose) {
-				log(`${PLUGIN_NAME}: Skipping unsupported file ${chalk.blue(file.relative)}`)
+	return through.obj(
+		{
+			maxConcurrency: 8,
+		},
+		(file, enc, cb) => {
+			if (file.isNull()) {
+				cb(null, file);
+				return;
 			}
 
-			return cb(null, file)
+			if (file.isStream()) {
+				return cb(new PluginError(PLUGIN_NAME, "Streaming not supported"));
+			}
+
+			if (validExts.indexOf(path.extname(file.path).toLowerCase()) === -1) {
+				opts.verbose > 1 && log(
+					`${PLUGIN_NAME}: Skipping unsupported file ${chalk.blue(
+						file.relative
+					)}`
+				);
+
+				return cb(null, file);
+			}
+
+			opts.verbose > 1 && log(`${PLUGIN_NAME}: Compiling file ${file.relative}`);
+
+			const data = gettextParser.po.parse(file.contents);
+			const output = gettextParser.mo.compile(data);
+
+			file.contents = output;
+			file.path = file.path.replace(/\.[^/.]+$/, ".mo");
+
+			totalFiles++;
+
+			opts.verbose > 0 && log(
+				PLUGIN_NAME + ":",
+				chalk.green("✔ ") + file.relative + chalk.gray(` successfully compiled`)
+			);
+
+			cb(null, file);
+		},
+		(cb) => {
+			let msg = `Compiled ${totalFiles} translation ${plur(
+				"file",
+				totalFiles
+			)}`;
+
+			opts.verbose > 0 && log(PLUGIN_NAME + ":", msg);
+			cb();
 		}
-
-    if (opts.verbose) {
-      log(`${PLUGIN_NAME}: Compiling file ${file.relative}`)
-    }
-
-		const data = gettextParser.po.parse(file.contents)
-    const output = gettextParser.mo.compile(data)
-
-    file.contents = output
-    file.path = file.path.replace(/\.[^/.]+$/, '.mo')
-
-    totalFiles++
-
-    log(PLUGIN_NAME + ':', chalk.green('✔ ') + file.relative + chalk.gray(` successfully compiled`));
-
-    cb(null, file)
-
-	}, cb => {
-
-		let msg = `Compiled ${totalFiles} translation ${plur('file', totalFiles)}`
-
-		log(PLUGIN_NAME + ':', msg)
-		cb()
-
-	})
-
-}
+	);
+};
